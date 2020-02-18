10c709c0a673ff1c50391528ac7f0a92db9a5870
rezan21 <reza.naghshineh@gmail.com>
2020-02-18 23:57:46 +0000

    plot 4 figures in css grid

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 
+
 app = Flask(__name__)
 
 def plot_cnfrmd_frame():
@@ -29,6 +30,98 @@ def plot_cnfrmd_frame():
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON # return JSON
 
+def plot_world_wide():
+    df = pd.read_csv("data/visuals_02.csv")
+    timestamp_grouped = df.groupby("timestamp").sum().sort_values("timestamp", ascending=True)
+    timestamp_grouped.reset_index(inplace=True)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=timestamp_grouped["timestamp"], y=timestamp_grouped["conf_cases"], name='Confirmed Cases',
+                            line=dict(width=3)))
+    fig.add_trace(go.Scatter(x=timestamp_grouped["timestamp"], y=timestamp_grouped["recov_cases"], name='Recovered Cases',
+                            line=dict(width=3)))
+    fig.add_trace(go.Scatter(x=timestamp_grouped["timestamp"], y=timestamp_grouped["death_cases"], name='Death Cases',
+                            line=dict(width=3)))
+    fig.update_layout(title='CoronaVirus Performance over Time (Worldwide)',
+                    xaxis_title='Time',
+                    yaxis_title='Number of Cases (Persons)')
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def plot_exc_china():
+    df = pd.read_csv("data/visuals_02.csv")
+    china = df[df["Country/Region"]=="Mainland China"].drop("Country/Region",axis=1)
+    timestamp_grouped_exc_china = df.drop(china.index).reset_index(drop=True)
+    timestamp_grouped_exc_china = timestamp_grouped_exc_china.groupby("timestamp").sum().sort_values("timestamp", ascending=True)
+    timestamp_grouped_exc_china.reset_index(inplace=True)
+
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=timestamp_grouped_exc_china["timestamp"], y=timestamp_grouped_exc_china["conf_cases"], name='Confirmed Cases',
+                            line=dict(width=3)))
+    fig.add_trace(go.Scatter(x=timestamp_grouped_exc_china["timestamp"], y=timestamp_grouped_exc_china["recov_cases"], name='Recovered Cases',
+                            line=dict(width=3)))
+    fig.add_trace(go.Scatter(x=timestamp_grouped_exc_china["timestamp"], y=timestamp_grouped_exc_china["death_cases"], name='Death Cases',
+                            line=dict(width=3)))
+
+    fig.update_layout(title='CoronaVirus Performance over Time (Excluding China)',
+                    xaxis_title='Time',
+                    yaxis_title='Number of Cases (Persons)')
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def plot_barchart():
+    df = pd.read_csv("data/visuals_02.csv")
+    # latest stat of each country
+    countries = df["Country/Region"].unique()
+    country_grouped = df.groupby("Country/Region") # group by country
+    countries_stat = []
+    for country in countries:
+        countries_stat.append(country_grouped.get_group(country).sort_values("timestamp", ascending=False).iloc[0]) # for each group get the latest stat
+
+    countries_stat = pd.DataFrame(countries_stat).reset_index(drop=True)
+    china = countries_stat[countries_stat["Country/Region"]=="Mainland China"].index
+    countries_stat.drop(china,axis=0,inplace=True)
+    other = countries_stat[countries_stat["Country/Region"]=="Others"].index
+    countries_stat.drop(other,axis=0,inplace=True)
+
+
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=countries_stat["Country/Region"],
+        y=countries_stat["conf_cases"],
+        name='Confirmed Cases',
+    # marker_color='indianred'
+    ))
+    fig.add_trace(go.Bar(
+        x=countries_stat["Country/Region"],
+        y=countries_stat["recov_cases"],
+        name='Recovered Cases',
+        marker_color='lightgreen',
+
+
+    ))
+    fig.add_trace(go.Bar(
+        x=countries_stat["Country/Region"],
+        y=countries_stat["death_cases"],
+        name='Death Cases',
+        marker_color='red',
+
+
+    ))
+
+    fig.update_layout(title='CoronaVirus: Ratios<br><span style="font-size:80%">Excluding China</span>',
+                    xaxis_title='Country',
+                    yaxis_title='Number of Cases (Persons)')
+    # Here we modify the tickangle of the xaxis, resulting in rotated labels.
+    fig.update_layout(barmode='group', xaxis_tickangle=-55)
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -36,7 +129,15 @@ def home():
 @app.route('/confirmed-cases')
 def confirmed_cases():
     cnfrmd_frame = plot_cnfrmd_frame()
-    return render_template('confirmed_cases.html', figure=cnfrmd_frame)
+    world_wide_plot= plot_world_wide()
+    exc_china_plot = plot_exc_china()
+    barchart_plot = plot_barchart()
+    return render_template('confirmed_cases.html',
+                            cnfrmd_frame=cnfrmd_frame,
+                            world_wide_plot=world_wide_plot,
+                            exc_china_plot=exc_china_plot,
+                            barchart_plot=barchart_plot
+                        )
 
 
 if __name__ == '__main__':
